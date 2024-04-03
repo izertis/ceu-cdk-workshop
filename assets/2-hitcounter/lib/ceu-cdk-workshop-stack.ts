6455707666c8e2ffe3a0df4e1baa96a18c582bf3
Edgar Alonso <ealonso@okode.com>
2024-04-03 19:52:25 +0200

    Fixed hello-world asset Lambda role. Added hitcounter assets

--- assets/2-hitcounter/lib/ceu-cdk-workshop-stack.ts
+++ assets/2-hitcounter/lib/ceu-cdk-workshop-stack.ts
@@ -3,8 +3,10 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Role } from 'aws-cdk-lib/aws-iam';
+import { HitCounter } from './hitcounter';
 
 export class CeuCdkWorkshopStack extends Stack {
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -12,11 +14,15 @@ export class CeuCdkWorkshopStack extends Stack {
       runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
       code: lambda.Code.fromAsset('lambda/hello-world'),  // code loaded from "lambda" directory
       handler: 'index.handler',                // file is "hello", function is "handler"
-      role: Role.fromRoleName(this, 'Role', 'LabRole')
+      role: Role.fromRoleName(this, 'Role', 'LabRole'),
+    });
+
+    const hitcounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: hitcounter.handler
     });
 
   }
