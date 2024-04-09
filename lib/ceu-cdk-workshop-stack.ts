4ae0d8db3e5a285b00af9e4121bde7fecfda9723
Edgar Alonso <ealonso@okode.com>
2024-04-09 12:46:20 +0200

    Removed resource

--- lib/ceu-cdk-workshop-stack.ts
+++ lib/ceu-cdk-workshop-stack.ts
@@ -3,32 +3,20 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import { Role } from 'aws-cdk-lib/aws-iam';
-import { HitCounter } from './hitcounter';
-import { TableViewer } from 'cdk-dynamo-table-viewer/lib/table-viewer';
 
 export class CeuCdkWorkshopStack extends Stack {
-
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_20_X,    // execution environment
-      code: lambda.Code.fromAsset('lambda/hello-world'),  // code loaded from "lambda" directory
-      handler: 'index.handler',                // file is "hello", function is "handler"
-      role: Role.fromRoleName(this, 'Role', 'LabRole'),
-    });
-
-    const hitcounter = new HitCounter(this, 'HelloHitCounter', {
-      downstream: hello
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda/hello-world'),
+      handler: 'index.handler',
+      role: Role.fromRoleName(this, 'Role', 'LabRole')
     });
 
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hitcounter.handler
-    });
-
-    new TableViewer(this, 'ViewHitCounter', {
-      title: 'Hello Hits',
-      table: hitcounter.table
+      handler: hello
     });
 
   }
